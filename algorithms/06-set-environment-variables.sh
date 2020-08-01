be94e26b4f249c07bb2872372fa6bab9d6b8ee5f
victor.arana <victor_arana@hotmail.com>
2020-08-01 13:40:31 -0500

    add path

--- algorithms/06-set-environment-variables.sh
+++ algorithms/06-set-environment-variables.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 JAVA_HOME=$(readlink -f /usr/bin/javac | sed 's:/bin/javac::')
-echo "export JAVA_HOME=$JAVA_HOME" >> .bashrc
+echo "export JAVA_HOME=$JAVA_HOME" >> ~/.bashrc
+PATH=$JAVA_HOME/bin:$PATH
+echo "export PATH=$PATH" >> ~/.bashrc
