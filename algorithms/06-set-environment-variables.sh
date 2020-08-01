7858ca555a1b286c4e014b66749f8cbaa0325fe7
victor.arana <victor_arana@hotmail.com>
2020-08-01 14:02:45 -0500

    add M2_HOME environment variable

--- algorithms/06-set-environment-variables.sh
+++ algorithms/06-set-environment-variables.sh
@@ -4,3 +4,8 @@ JAVA_HOME=$(readlink -f /usr/bin/javac | sed 's:/bin/javac::')
 echo "export JAVA_HOME=$JAVA_HOME" >> ~/.bashrc
 PATH=$JAVA_HOME/bin:$PATH
 echo "export PATH=$PATH" >> ~/.bashrc
+
+M2_HOME=$(readlink -f /usr/bin/mvn | sed 's:/bin/mvn::')
+echo "export M2_HOME=$M2_HOME" >> ~/.bashrc
+PATH=$M2_HOME/bin:$PATH
+echo "export PATH=$PATH" >> ~/.bashrc
