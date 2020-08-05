32d28a525ffc1886a7534c31ef08f9e5c27f4f1b
victor.arana <victor_arana@hotmail.com>
2020-08-05 09:45:05 -0500

    install intelliJ idea

--- algorithms/01-install-java.sh
+++ algorithms/01-install-java.sh
@@ -23,6 +23,17 @@ echo "export PATH=$PATH" >> ~/.bashrc
 # Install curl
 sudo apt install -y curl
 
+# Install IntelliJ
+sudo snap install -y intellij-idea-community --channel=2020.1/stable --classic
+
+cd
+sudo rm -rf .cache/JetBrains/IdeaIC2020.1/
+sudo rm -rf .config/JetBrains/IdeaIC2020.1/
+sudo rm -rf .local/share/JetBrains/IdeaIC2020.1/
+sudo curl -O "https://lift.cs.princeton.edu/java/linux/IdeaIC2020.1.zip"
+sudo unzip IdeaIC2020.1.zip
+sudo rm IdeaIC2020.1.zip
+
 # Install command line tools
 cd /usr/local
 sudo curl -O "https://lift.cs.princeton.edu/java/linux/lift-cli.zip"
