0d451d4fdf417593cb535aaf4e7f5bc7136ae91a
victor.arana <victor_arana@hotmail.com>
2020-08-05 09:16:55 -0500

    Merge install scripts

--- algorithms/01-install-java.sh
+++ algorithms/01-install-java.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
+# Update index
+sudo apt-get update
+
+# Install Java JDK
 sudo apt install -y default-jdk
+
+# Install Maven
+sudo apt install -y maven
+
+# Set environment variables
+JAVA_HOME=$(readlink -f /usr/bin/javac | sed 's:/bin/javac::')
+echo "export JAVA_HOME=$JAVA_HOME" >> ~/.bashrc
+PATH=$JAVA_HOME/bin:$PATH
+echo "export PATH=$PATH" >> ~/.bashrc
+
+M2_HOME=$(readlink -f /usr/bin/mvn | sed 's:/bin/mvn::')
+echo "export M2_HOME=$M2_HOME" >> ~/.bashrc
+PATH=$M2_HOME/bin:$PATH
+echo "export PATH=$PATH" >> ~/.bashrc
+
+# Install curl
+sudo apt install -y curl
+
+# Install command line tools
+cd /usr/local
+sudo curl -O "https://lift.cs.princeton.edu/java/linux/lift-cli.zip"
+sudo unzip lift-cli.zip
+sudo rm lift-cli.zip
+
+
