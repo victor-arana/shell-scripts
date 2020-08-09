a9a6c03977353023a39b57cc3c052a8a75f532e8
victor.arana <victor_arana@hotmail.com>
2020-08-09 13:28:44 -0500

    rearrange scripts

---
+++ apis-integration/00-main-script.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+./01-update-repository.sh
+./02-install-java-jdk.sh
+./03-install-maven.sh
+./04-set-environment-variables.sh
+./05-install-jmeter.sh
+./06-install-curl.sh
+./07-install-nodejs.sh
+./08-install-angular-cli.sh
+./09-install-heroku-cli.sh
+./10-install-vim.sh
+./11-install-sts.sh
+./12-install-postman.sh
+./13-install-vscode.sh
---
+++ apis-integration/04-set-environment-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set environment variables
+JAVA_HOME=$(readlink -f /usr/bin/javac | sed 's:/bin/javac::')
+echo "export JAVA_HOME=$JAVA_HOME" >> ~/.bashrc
+PATH=$JAVA_HOME/bin:$PATH
+echo "export PATH=$PATH" >> ~/.bashrc
+
+M2_HOME=$(readlink -f /usr/bin/mvn | sed 's:/bin/mvn::')
+echo "export M2_HOME=$M2_HOME" >> ~/.bashrc
+PATH=$M2_HOME/bin:$PATH
+echo "export PATH=$PATH" >> ~/.bashrc
+
