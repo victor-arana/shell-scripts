5ee44d4a7345d20c0cfd16a38ed6b109534611f2
victor.arana <victor_arana@hotmail.com>
2020-11-24 19:38:02 -0600

    :git: add git installation script
    Changes committed:
    	new file:   apache-xalan/00-main-script.sh
    	new file:   install-scripts/14-install-git.sh

---
+++ apache-xalan/00-main-script.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+#../install-scripts/00-update-repository.sh
+#../install-scripts/10-install-vim.sh
+../install-scripts/14-install-git.sh
---
+++ install-scripts/14-install-git.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt install git
+git config --global user.name "victor.arana"
+git config --global user.email victor_arana@hotmail.com
+git config --global core.editor "vim"
