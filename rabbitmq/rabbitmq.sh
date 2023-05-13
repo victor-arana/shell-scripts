b43673b4d800add6195b783be2190760fd58acbb
victor.arana <victor_arana@hotmail.com>
2023-05-13 02:37:03 -0600

    :rabbitmq: add rabbitmq install script

--- rabbitmq/rabbitmq.sh
+++ rabbitmq/rabbitmq.sh
@@ -7,3 +7,4 @@
 ../install-scripts/02-install-java-jdk-17.sh
 ../install-scripts/19-install-gradle-7.5.1.sh
 ../install-scripts/04-install-intellij-idea-2022.3.sh
+../install-scripts/20-install-rabbitmq-3.11.2.sh
