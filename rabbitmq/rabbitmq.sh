23434f0bfe03ece34b32d01d16768f99e65ad945
victor.arana <victor_arana@hotmail.com>
2023-05-12 22:10:16 -0600

    :rabbitmq: install java and vim

--- rabbitmq/rabbitmq.sh
+++ rabbitmq/rabbitmq.sh
@@ -3,3 +3,5 @@
 #Update the index
 ../install-scripts/01-update-repository.sh
 ../install-scripts/03-install-chrome.sh
+../install-scripts/10-install-vim.sh
+../install-scripts/02-install-java-jdk-17.sh
